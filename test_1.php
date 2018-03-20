82196c40cabfbfadc969ba56a81f584229369632
tianshuyu <990979281@qq.com>
2018-03-20 14:27:45 +0800

    web-ceyan

---
+++ test_1.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>array_practice</title>
+</head>
+
+<body>
+	<?php
+	$a=array(9,8,5,6);
+	list($dc,$dd)=$a;
+	
+	//shuffle($array);
+	
+	echo "<br>";
+	print_r($dc);
+	echo "<br>";
+	print_r($dd);
+	?>
+	
+</body>
+</html>
